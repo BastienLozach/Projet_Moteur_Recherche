a26e9e66ba3203a3be56de6bf6fe76c7c6071f80
mathieu <mathieu.boye28@gmail.com>
2018-03-20 14:37:04 +0100

    Ajout affichage

--- search_show.py
+++ search_show.py
@@ -2,6 +2,7 @@ import json
 import sys
 import os
 from itertools import islice
+import email
 
 
 historyFolder = "history"
@@ -19,20 +20,23 @@ index=0
 print(searchedTerm)
 ##search in history
 try :
-    with open(os.path.join(historyFolder, searchedTerm)) as file :
+    with open(os.path.join(historyFolder, searchedTerm + ".json")) as file :
         response = json.load(file)
-        for key, value in islice(response.iteritems(), 10):
+        for mailFileName, nbOccurences in islice(response.items(), 10):
             index+=1
-            print(key, value)
-            #print("Resultat n° " + index + "\n")
-            #print("\tExpediteur : " + resultat["exp"] + "\n")
-            #print("\tDestinataire : " + resultat["dest"] + "\n")
-            #print("\tSujet : " + resultat["sujet"] + "\n")
-            #print("\tMessage : " + resultat["message"] + "\n")
-            #print("\Occurences : " + resultat["occurences"] + "\n")
+            msg = email.message_from_file(open(mailFileName))
+            print("Resultat n° " + str(index) + "\n")
+            if msg['From']:
+                print("\tExpediteur : " + msg["From"])
+            if msg['To']:
+                destList = msg["To"].split(",")
+                print("\tDestinataire " + ("(Total " + str(len(destList)) + ")" if len(destList) > 10 else "" ) +": " + ",".join(islice(destList, 10)) + ( ", ..." if len(destList) > 10 else "" ))
+            if msg['Subject']:
+                print("\tSujet : " + msg["Subject"])
+            print("\tOccurences : " ,nbOccurences, "\n")
             
             
-except :
+except IOError:
     print("Erreur")
         
-print(response)
+#print(response)

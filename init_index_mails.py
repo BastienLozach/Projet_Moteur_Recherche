88f5ecf924f03f50556db8eb2986a28636996d2f
Kaylimo <jp.turowicz@gmail.com>
2018-03-22 11:52:34 +0100

    Indexation (codage de la fonctionnalité terminé, à lancer pour obtenir les index)

--- init_index_mails.py
+++ init_index_mails.py
@@ -21,10 +21,6 @@ if not os.path.exists(outputFile4or5):
 if not os.path.exists(outputFile5):
     os.makedirs(outputFile5)
 
-index_less_than_four_letters = {}
-index_more_than_five_letters = {}
-index_four_or_five_letters = {}
-
 def index_four_or_five_letters_insert(mot, nb, cibleMail) :
 
     index = {}
@@ -41,9 +37,41 @@ def index_four_or_five_letters_insert(mot, nb, cibleMail) :
     with open(os.path.join(outputFile4or5, mot[0] + ".json"), "w") as file :
         json.dump(index, file, indent=4)
 
+def index_less_than_four_letters_insert(mot, nb, cibleMail) :
+
+    index = {}
+    if os.path.exists(os.path.join(outputFile4, mot[0] + ".json")):
+        with open(os.path.join(outputFile4, mot[0] + ".json")) as file :
+            index = json.load(file)
+
+    if mot in index :
+        index[mot].append({cibleMail : nb})
+    else :
+        value = {mot : [{cibleMail : nb}]}
+        index.update(value)
+
+    with open(os.path.join(outputFile4, mot[0] + ".json"), "w") as file :
+        json.dump(index, file, indent=4)
+
+def index_more_than_five_letters_insert(mot, nb, cibleMail) :
+
+    index = {}
+    if os.path.exists(os.path.join(outputFile5, mot[0] + ".json")):
+        with open(os.path.join(outputFile5, mot[0] + ".json")) as file :
+            index = json.load(file)
+
+    if mot in index :
+        index[mot].append({cibleMail : nb})
+    else :
+        value = {mot : [{cibleMail : nb}]}
+        index.update(value)
+
+    with open(os.path.join(outputFile5, mot[0] + ".json"), "w") as file :
+        json.dump(index, file, indent=4)
+
 for folder, subs, files in os.walk(inputFile):
         for mailFile in files:
-            ##try :
+            try :
                 cibleMail = os.path.join(folder, mailFile)
                 mail = open(cibleMail).read()
                 b = email.message_from_string(mail).get_payload()
@@ -51,21 +79,19 @@ for folder, subs, files in os.walk(inputFile):
                 if msg['Subject']:
                     Subject = msg['Subject']
                     b = Subject + " " + b
-                #arr = re.split("[\\t\\n\\r\'\"\.,;:!\?\\/ ]", b)
                 arr = re.split("\W+", b)
+                arr = [mot.lower() for mot in arr]
                 counter = collections.Counter(arr)
                 for mot,nb in counter.items():
                     print(mot)
-                    if len(mot) < 4 :
-                        #index_less_than_four_letters_insert(mot, nb, cibleMail)
-                        a = "do something"
+                    if 0 < len(mot) < 4 :
+                        index_less_than_four_letters_insert(mot, nb, cibleMail)
                     elif len(mot) > 5 :
-                        #index_more_than_five_letters_insert(mot, nb, cibleMail)
-                        a = "do something"
-                    else :
+                        index_more_than_five_letters_insert(mot, nb, cibleMail)
+                    elif 3 < len(mot) < 6 :
                         index_four_or_five_letters_insert(mot,nb, cibleMail)
-            ##except IOError:
-                ##print(IOError)
+            except IOError:
+                print(IOError)
 
 
 
